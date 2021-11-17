08c865cc0a39513df6378cee36e136de3fc16bda
Abraham Ne <34808002+AbrahamNe@users.noreply.github.com>
2021-11-17 10:06:04 -0600

    Jenkis tutorial 1st

---
+++ LastElementOfaStream.java
@@ -0,0 +1,28 @@
+package Java100DaysChallenge;
+
+import java.util.List;
+
+public class LastElementOfaStream {
+
+	public static void main(String[] args) {
+
+   List<String> filetypes = List.of("jpg","png","docx");
+   solution(filetypes);
+
+	}
+	
+	public static void solution(List<String>  list) {
+		
+	String output = list.stream().reduce( (e1, e2) -> e2).get();
+	
+	// using skip
+	
+	String lastElement = list.stream()
+			.skip(list.size() -1)
+			.findFirst()
+			.get();
+	System.out.println(lastElement);
+	System.out.println("Hello from Jenkins");
+	}
+
+}
